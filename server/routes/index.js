8b1221c7a988af6fb403eee0061a8163638ce3a2
Katy <ihatetoast@gmail.com>
2018-04-12 13:38:20 -0500

    ready maaaybe for building the components for auth

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 const personRoutes = require('./api/persons');
+const apiRoutes = require('./api/api');
 
-// Person routes
+// API Routes
+router.use('/api', apiRoutes);
+
+// Person routes 'api/persons' ?
 router.use('/persons', personRoutes);
 
 module.exports = router;
